a0f5b5d0b97f1280047eed4613bd5f547e4c57b4
tattianeoliveira14 <130249961+tattianeoliveira14@users.noreply.github.com>
2025-10-02 22:32:46 -0300

    Agendamento

---
+++ src/entities/agendamento.entity.ts
@@ -0,0 +1,38 @@
+!	import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+  DOCTOR = 'doctor',
+  AGENDA = 'agenda'
+}
+
+@Entity()
+export class Agenda {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  paciente: string;
+
+  @Column()
+  telefone: string;
+
+  @Column()
+  medico: string;
+
+  @Column()
+  especialidade: string;
+  
+  @Column()
+  data: Date;
+
+  @Column()
+  horario: string;
+
+  @Column()
+  observacoes: string;
+  
+  @Column()
+  status: string;
+}
